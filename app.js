1f1d11ff0a11c8e56526ae9ecccfcd190f7ade47
Ankur Bansal <ankur.b1999@gmail.com>
2020-07-06 13:37:32 +0000

    resized images

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get("/", function(req, res){
+	res.render("home.ejs");
+});
 
 app.get("/gallery", function(req, res){
 	res.render("gallery.ejs");
